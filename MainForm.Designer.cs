889ca7c3f2121cc3d40bf64dea1853c9c9048852
Nešpor Lukáš <lukasnespor@gmail.com>
2022-11-09 17:41:36 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Awake
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.message = new System.Windows.Forms.Label();
+            this.NotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // MainTimer
+            // 
+            this.MainTimer.Enabled = true;
+            this.MainTimer.Interval = 1000;
+            this.MainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // message
+            // 
+            this.message.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.message.AutoSize = true;
+            this.message.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.message.Location = new System.Drawing.Point(22, 20);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(215, 31);
+            this.message.TabIndex = 0;
+            this.message.Text = "Neaktivita: 0 min";
+            // 
+            // NotifyIcon
+            // 
+            this.NotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("NotifyIcon.Icon")));
+            this.NotifyIcon.Text = "Nespi";
+            this.NotifyIcon.Visible = true;
+            this.NotifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_Click);
+            this.NotifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 69);
+            this.Controls.Add(this.message);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Nespi";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer MainTimer;
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.NotifyIcon NotifyIcon;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Awake
+{
+    public partial class MainForm : Form
+    {
+        int inactiveSeconds = 0;
+        Point lastPosition = Cursor.Position;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Cursor = new Cursor(Cursor.Current.Handle);
+        }
+
+        void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+                Hide();
+        }
+
+        void NotifyIcon_Click(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        void MainTimer_Tick(object sender, EventArgs e)
+        {
+            var currentPosition = Cursor.Position;
+
+            if (lastPosition.X == currentPosition.X && lastPosition.Y == currentPosition.Y)
+                SetInactiveSecondsAndText(++inactiveSeconds);
+            else
+            {
+                lastPosition = currentPosition;
+                SetInactiveSecondsAndText(0);
+                return;
+            }
+
+            if (inactiveSeconds >= (5 * 60))
+                MoveCursor();
+        }
+
+        void SetInactiveSecondsAndText(int value)
+        {
+            inactiveSeconds = value;
+            message.Text = $"Neaktivita: {value / 60} min";
+        }
+
+        void MoveCursor()
+        {
+            var currentPosition = Cursor.Position;
+            Cursor.Position = new Point(currentPosition.X + 10, currentPosition.Y + 10);
+            Cursor.Position = new Point(currentPosition.X, currentPosition.Y);
+        }
+    }
+}
